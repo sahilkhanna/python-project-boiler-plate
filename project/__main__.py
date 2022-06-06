2dd2ccb90d96f14fac25b287bd93adca0a95eca9
andyvorld <andyvorld@live.com.au>
2022-06-06 21:07:01 +1000

    __main__.py script exec

--- project/__main__.py
+++ project/__main__.py
@@ -1,3 +1,11 @@
+if __name__ == "__main__" and __package__ is None:
+    print("Called from __main__.py as script")
+    import sys
+    import pathlib
+    sys.path.append(str(pathlib.Path(__file__).parent.parent.resolve()))
+
+    __package__ = "project"
+
 from project import Project
 
 def main():
